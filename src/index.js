f6126aa4eaedf615cc4052ce2c8eb8895bf93bc2
wliang <wliang@infervision.com>
2021-07-04 22:30:59 +0800

    [add] init code

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// const element = <h1 title="foo">Hello</h1>
+// const containor = document.getElementById("root")
+// ReactDOM.render(element, containor)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const element = {
+  type: 'h1',
+  props: {
+    title: 'foo',
+    children: 'hello'
+  }
+}
+const containor = document.getElementById("root")
+
+// 将element对象创建node
+const node = document.createElement(element.type)
+node.title = element.props.title
+const text = document.createTextNode("")
+text['nodeValue'] = element.props.children
+// 添加元素
+node.appendChild(text);
+containor.appendChild(node)
+
+
